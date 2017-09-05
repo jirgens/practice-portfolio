32487a38870d3e17ad1863140ddd90393f66fa5e
jirgens <jessa.irg@gmail.com>
2017-09-05 13:00:23 -0700

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,42 @@ module ApplicationHelper
   def copyright_generator
       DaymakerViewTool::Renderer.copyright 'Daymaker Digital', 'All rights reserved'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+      {
+        url: portfolios_path,
+        title: "Portfolio"
+      },
+      {
+        url: blogs_path,
+        title: "Blog"
+      },
+      {
+        url: about_path,
+        title: "About"
+      },
+      {
+        url: contact_path,
+        title: "Contact"
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
 end
