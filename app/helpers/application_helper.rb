2935f7627905f81705ec112d5e0b62300313bbc6
jirgens <jessa.irg@gmail.com>
2017-10-27 16:13:37 -0500

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,4 +57,17 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Daymaker Digital", sticky: false)
+  end
+
+
 end
