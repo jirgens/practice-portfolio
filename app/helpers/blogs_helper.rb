3558ca528020e74946293796c1a6a840786395c0
jirgens <jessa.irg@gmail.com>
2017-11-05 13:34:12 -0800

    Implemented partial for admin actions

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,10 @@ module BlogsHelper
   def gravatar_helper user
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 50
   end
+
+  def blog_status_color blog
+    if blog.draft?
+      'color: red;'
+    end
+  end
 end
